8aaaeef7a1fc5f4afd2efbe8a406f702e44477b2
Khai <khai.44.13.572@gmail.com>
2026-01-09 15:27:17 +0300

    Changed come things.

--- src/main/java/net/khai/tutormod/TutorMod.java
+++ src/main/java/net/khai/tutormod/TutorMod.java
@@ -1,9 +1,10 @@
 package net.khai.tutormod;
 
+import net.khai.tutormod.block.ModBlocks;
 import org.slf4j.Logger;
-import com.mojang.logging.LogUtils;
+import net.khai.tutormod.item.ModItems;
 
-import net.khai.tutormod.item.ModItems; // Импорт ваших предметов
+import com.mojang.logging.LogUtils;
 
 import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.api.distmarker.Dist;
@@ -19,45 +20,63 @@ import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(TutorMod.MOD_ID)
 public class TutorMod {
     public static final String MOD_ID = "tutormod";
     public static final Logger LOGGER = LogUtils.getLogger();
 
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public TutorMod(IEventBus modEventBus, ModContainer modContainer) {
-        // РЕГИСТРАЦИЯ ПРЕДМЕТОВ (Этого не хватало)
-        ModItems.register(modEventBus);
-
+        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
-        // РЕГИСТРАЦИЯ ВКЛАДКИ КРЕАТИВА
+        // Register our items
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
+
     }
 
+    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        // Проверка на вкладку Ингредиенты
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            // Добавляем предметы через .get()
             event.accept(ModItems.BISMUTH.get());
             event.accept(ModItems.RAW_BISMUTH.get());
         }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
+        }
     }
 
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
+
     }
 
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @EventBusSubscriber(modid = TutorMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     static class ClientModEvents {
         @SubscribeEvent
         static void onClientSetup(FMLClientSetupEvent event) {
+
         }
     }
 }
